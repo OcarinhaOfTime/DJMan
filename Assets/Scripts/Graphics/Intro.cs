1dccd2aa75537bbc96e7fc5dbe99ed1444546312
OcarinhaOfTime <vagnerseibert@gmail.com>
2018-02-28 14:06:03 -0300

    Added intro

---
+++ Assets/Scripts/Graphics/Intro.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Intro : MonoBehaviour {
+    public Graphic pressAnyKey;
+    public float blinkSpeed = 1;
+
+    private void Update() {
+        var c = pressAnyKey.color;
+        var t = Mathf.PingPong(Time.time * blinkSpeed, 1);
+        c.a = Mathf.SmoothStep(0, 1, t);
+        pressAnyKey.color = c;
+
+        if (Input.anyKeyDown) {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
